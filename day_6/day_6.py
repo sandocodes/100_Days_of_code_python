b67d1f69dcca9aa598962c64665b3fe23f4a727d
Justin <codewithjustin@yahoo.com>
2024-01-11 22:02:53 -0500

    Day 7

---
+++ day_6/day_6.py
@@ -0,0 +1,26 @@
+# Day 6: Functions, While Loops
+
+# Functions:
+# Syntax:
+#      def my_function():
+#           print(something)
+# 
+# Call or trigger function:
+#      my_function()
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
+
+# While Loop:
+# Syntax:
+#       while condition is met:
+#             Do something repeatedly
+# 
+# 
+
+"""
+Day 6 Project was solving a Maze Challenge. Must be completed after Day 15!!
+"""
