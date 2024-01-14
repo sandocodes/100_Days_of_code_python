cf270e186fe004216d554bb9b1c967a43dbb1674
Justin <codewithjustin@yahoo.com>
2024-01-13 23:52:22 -0500

    Updated Day 7
    Added hangman.py file in day_7/project folder.

--- day_7/day_7.py
+++ day_7/day_7.py
@@ -1,7 +1,1 @@
-# Hangman Game
-import random
-from day_7.project.hangman_words import word_list
-from hangman_art import logo, stages
 
-# Display Hangman logo
-print(logo)
---
+++ day_7/project/hangman.py
@@ -0,0 +1,7 @@
+# Hangman Game
+import random
+from day_7.project.hangman_words import word_list
+from hangman_art import logo, stages
+
+# Display Hangman logo
+print(logo)
