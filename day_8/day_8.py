0f330b4d0357cb627597d945c56bbdcd128ff740
Justin <codewithjustin@yahoo.com>
2024-01-14 23:14:40 -0500

    caesar_cipher.py
    Wrote both the encrypt and decrypt functions of the caesar cipher. Also added an if statement to check if the user wants to 'encode' or 'decode' the text.

--- day_8/day_8.py
+++ day_8/day_8.py
@@ -1,1 +1,31 @@
 # Function Parameters & Project: Caesar Cipher
+# def greet():
+#     print("Hello")
+#     print("How are you?")
+#     print("Have a nice day!")
+# greet()
+
+# def greet_with_name(name):
+#     print(f"Hello, {name}")
+#     print(f"How are you, {name}?")
+#     print(f"Have a nice day, {name}!")
+# greet_with_name("Billie")
+
+# In programming, when we give a function a parameter, we are essentially creating a variable with the name of the parameter, with the value set to the argument that's passed to the function call. For example, in the greet_with_name function, the function has a parameter called name and an argument called "Billie". This is essentially equivalent to name = "Billie".
+
+# Multiple Parameters to a function: A function can take any amount of parameters
+def greet_with(name, location):
+    print(f"Hello {name}.")
+    print(f"What is it like in {location}?")
+
+# greet_with("Jack Bauer", "Boston") # ==> This is the default
+
+
+# Keyword Arguments:
+# greet_with(location="Boston", name="Jack Bauer")
+
+
+# Test Code:
+for i, j in enumerate(['bar', 'foo', 'bar', 'baz', 'bar']):
+    if j == 'bar':
+        print(i)
--- day_8/project/caesar_cipher.py
+++ day_8/project/caesar_cipher.py
@@ -1,2 +1,46 @@
+"""
+Day4 Project: Caesar Cipher
+Written By: Justin Sando Kollie
+Description: 
+Date: January 9, 2024
+Project Source: The source of this project is from a Udemy Course: 100 Days of Code: The Complete Python Pro Bootcamp for 2023
+"""
+
+
 from art import logo
 print(logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+# Create a function that has two parameters: plain_text and shift_amount
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    # Shift each letter of the 'plain_text' forwards in the alphabet list by the shift_amount and print the encrypted text
+    for letter in plain_text:
+        position = alphabet.index(letter) #Get the position of each of the letters in 'plain_text' in correspondence to the alphabet list
+        new_position = position + shift_amount #Shift each letter in the 'plain_text' to a new position in the 'alphabet' list using the value of the shift amount
+        new_letter = alphabet[new_position] #Using the new position just generated, get it's corresponding letter in the 'alphabet' list
+        cipher_text += new_letter
+
+    print(f"The encrypted text is: {cipher_text}")
+
+# Write the decrypted verson
+def decrypt(cipher_text, shift_amount):
+    plain_text = ""
+    for letter in cipher_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        real_letter = alphabet[new_position]
+        plain_text += real_letter
+
+    print(f"The plain text is: {plain_text}")
+
+
+if direction == "encode":
+    encrypt(shift_amount=shift, plain_text=text)
+elif direction == "decode":
+    decrypt(shift_amount=shift, cipher_text=text)
