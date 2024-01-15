900835cef20ad6fc485a92307fab7f6dc55bd68c
Justin <codewithjustin@yahoo.com>
2024-01-15 00:20:36 -0500

    Update caesar_cipher.py
    Added more logics like asking the user if they want to continue 'encoding' and 'decoding'. Also applied logic to cipher to check if the user enters letters to be encrypted or decrypted. If letters, encrypt or decrypt them. If number or symbols are included, only encrypt or decrypt the letters and keep the numbers or symbols as is. The shift position was also modified to ensure that whatever number the user enters, it doesn't go out of the 'alphabets' list range. For example, if the user enters 86, the shift will return the reminder of the shift value divided by the number of items in the 'alphabets' list.

--- day_8/project/caesar_cipher.py
+++ day_8/project/caesar_cipher.py
@@ -8,26 +8,41 @@ Project Source: The source of this project is from a Udemy Course: 100 Days of C
 
 
 from art import logo
+
+
+# Display Caesar Cipher Logo
 print(logo)
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 
-def caesar_cipher(start_text, shift_amount, cipher_direction):
-    end_text = ""
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-    if cipher_direction == "decode":
-        shift_amount *= -1
+should_continue = True
 
-    for letter in start_text:
-        position = alphabet.index(letter) 
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
-    print(f"The {cipher_direction}d text is {end_text}")
+    def caesar_cipher(start_text, shift_amount, cipher_direction):
+        end_text = ""
+        
+        if cipher_direction == "decode":
+            shift_amount *= -1
 
-caesar_cipher(start_text=text, shift_amount=shift, cipher_direction=direction)
+        for char in start_text:
+            if char in alphabets:
+                position = alphabets.index(char) 
+                new_position = position + shift_amount
+                end_text += alphabets[new_position]
+            else:
+                end_text += char
+        print(f"The {cipher_direction}d text is: {end_text}")
+
+    shift = round(shift & len(alphabets))
+    caesar_cipher(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    question = str(input("Type 'yes' if you want to continue. Otherwise type 'no'.\n")).lower()
+    if question == "no":
+        should_continue = False
+        print("Goodbye")
