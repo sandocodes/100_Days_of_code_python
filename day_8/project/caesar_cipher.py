b13136b70f78214e4980f82fe0fca8da5e4f618d
Justin <codewithjustin@yahoo.com>
2024-01-14 23:40:15 -0500

    Updated caesar_cipher.py
    Cleaned up the code to reduce redundancies.

--- day_8/project/caesar_cipher.py
+++ day_8/project/caesar_cipher.py
@@ -16,31 +16,18 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-# Create a function that has two parameters: plain_text and shift_amount
-def encrypt(plain_text, shift_amount):
-    cipher_text = ""
-    # Shift each letter of the 'plain_text' forwards in the alphabet list by the shift_amount and print the encrypted text
-    for letter in plain_text:
-        position = alphabet.index(letter) #Get the position of each of the letters in 'plain_text' in correspondence to the alphabet list
-        new_position = position + shift_amount #Shift each letter in the 'plain_text' to a new position in the 'alphabet' list using the value of the shift amount
-        new_letter = alphabet[new_position] #Using the new position just generated, get it's corresponding letter in the 'alphabet' list
-        cipher_text += new_letter
 
-    print(f"The encrypted text is: {cipher_text}")
+def caesar_cipher(start_text, shift_amount, cipher_direction):
+    end_text = ""
 
-# Write the decrypted verson
-def decrypt(cipher_text, shift_amount):
-    plain_text = ""
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        real_letter = alphabet[new_position]
-        plain_text += real_letter
+    if cipher_direction == "decode":
+        shift_amount *= -1
 
-    print(f"The plain text is: {plain_text}")
+    for letter in start_text:
+        position = alphabet.index(letter) 
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
 
+    print(f"The {cipher_direction}d text is {end_text}")
 
-if direction == "encode":
-    encrypt(shift_amount=shift, plain_text=text)
-elif direction == "decode":
-    decrypt(shift_amount=shift, cipher_text=text)
+caesar_cipher(start_text=text, shift_amount=shift, cipher_direction=direction)
