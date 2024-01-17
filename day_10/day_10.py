8889e47c4295d28eb8784889de1e09a17f5e7fca
Justin <codewithjustin@yahoo.com>
2024-01-17 18:44:50 -0500

    Day 10
    Code not working. Therefore, I reboot computer.

---
+++ day_10/day_10.py
@@ -0,0 +1,48 @@
+############### Functions with Outputs ###############
+# def format_name(f_name, l_name): 
+#     formated_f_name = f_name.title()
+#     formated_l_name = l_name.title() 
+    
+#     return f"{formated_f_name} {formated_l_name}"
+
+# first = input("First name: ")
+# last = input("Last name: ")
+
+# formated_string = format_name(first, last)
+
+# print(formated_string)
+
+
+
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return "True"
+            else:
+                return "False"
+        else:
+            return "True"
+    else:
+        return "False"
+  
+# TODO: Add more code here 👇
+def days_in_month(yr, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    leap = False
+    if month != 2:
+        return month_days[month - 1]
+    
+    if is_leap(yr):
+        return 29
+    else:
+        return month_days[month - 1]
+    
+    
+    # return month_days[month - 1]
+  
+#🚨 Do NOT change any of the code below 
+year = int(input("Year: ")) # Enter a year
+month = int(input("Month in number: ")) # Enter a month
+days = days_in_month(year, month)
+print(days)
---
+++ day_10/project/calculator_app.py
@@ -0,0 +1,5 @@
+from art import logo
+
+
+# Display
+print(logo)
