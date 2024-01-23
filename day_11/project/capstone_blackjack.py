128ad14214249205d960855f2e68ad2bdb6cdece
Justin <codewithjustin@yahoo.com>
2024-01-22 23:59:16 -0500

    Update capstone_blackjack.py
    Completed Blackjack (21) Game.

--- day_11/project/capstone_blackjack.py
+++ day_11/project/capstone_blackjack.py
@@ -5,15 +5,9 @@ Description/Rule: Add up your card or number (in our case) without going over 21
 Date: January 19, 2024
 Project Source: The source of this project is from a Udemy Course: 100 Days of Code: The Complete Python Pro Bootcamp for 2023
 """
-import random
+import random, os
 from art import logo
 
-# Display Project Logo
-print(logo)
-
-
-user_cards = []
-computer_cards = []
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
@@ -23,20 +17,10 @@ def deal_card():
     card = random.choice(cards)
     return card
 
-#
-for _ in range(2):
-    user_cards.append(deal_card())
-    computer_cards.append(deal_card())
-
-# For testing purpose
-print(f"user cards: {user_cards}")
-print(f"computer cards: {computer_cards}")
-
-
-# Calculate Scores
+# Deal the user and computer 2 cards each using deal_card()
+#6 Calculate Scores
 def calculate_score(cards):
-    """ Takes a list of cards and return the score calculated from the cards """
-
+    """Takes a list of cards and return the score calculated from the cards"""
     # Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
     if sum(cards) == 21 and len(cards) == 2:
         return 0
@@ -45,28 +29,65 @@ def calculate_score(cards):
     if 11 in cards and sum(cards) > 21:
         cards.remove(11)
         cards.append(1)
+
     return sum(cards)
 
+def compare(user_score, computer_score):
+    if user_score == computer_score:
+        return "Draw"
+    elif computer_score == 0:
+        return "Lose, opponent has a Blackjack"
+    elif user_score == 0:
+        return "Win with a Blackjack"
+    elif user_score > 21:
+        return "You went over. You lose"
+    elif computer_score > 21:
+        return "Opponent wen over. You win"
+    elif user_score > computer_score:
+        return "You win"
+    else:
+        return "You lose"
 
-#Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
+def play_game():
+
+    # Display Project Logo
+    print(logo)
+
+    user_cards = []
+    computer_cards = []
+    is_game_over = False
+
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
 
 
+    #Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
+    while not is_game_over:
+
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+        print(f"Your cards: {user_cards}, current score: {user_score}")
+        print(f"Computer's first card: {computer_cards[0]}")
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
+        else:
+            user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+            if user_should_deal == "y":
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
+
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_card())
+        computer_score = calculate_score(computer_cards)
+
+    print(f"Your final hand is: {user_cards}, final score: {user_score}")
+    print(f"Computer 's final hand: {computer_cards}, final score: {computer_score}")
+    print(compare(user_score, computer_score))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-# user_score = calculate_score(user_cards)
-# computer_score = calculate_score(computer_cards)
-
-# print(f"user score: {user_score}")
-# print(f"computer score: {computer_score}")
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+    os.system("clear")
+    play_game()
