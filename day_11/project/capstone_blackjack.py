55ae204d60df94d82777cfdd524a7ad281af8e85
Justin <codewithjustin@yahoo.com>
2024-01-22 19:10:11 -0500

    Update capstone_blackjack.py

--- day_11/project/capstone_blackjack.py
+++ day_11/project/capstone_blackjack.py
@@ -23,7 +23,7 @@ def deal_card():
     card = random.choice(cards)
     return card
 
-
+#
 for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
@@ -37,10 +37,18 @@ print(f"computer cards: {computer_cards}")
 def calculate_score(cards):
     """ Takes a list of cards and return the score calculated from the cards """
 
-
+    # Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
     if sum(cards) == 21 and len(cards) == 2:
         return 0
 
+    #Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
+
+#Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
 
 
 
