9ecb30a74cc67863c80a0cf4f9f75489faa7e87d
Justin <codewithjustin@yahoo.com>
2024-01-25 13:59:04 -0500

    update test_file.py
    Just testing code to fix bug in coffee_machine.py

--- day_15/project/test_file.py
+++ day_15/project/test_file.py
@@ -1,13 +1,19 @@
 # Testing
 import data
+
+resources = {
+    "water": 100,
+    "milk": 200,
+    "coffee": 100,
+}
 user_input = input("Enter an item: ")
 order_ingredients = data.MENU[user_input]["ingredients"]
-
+is_sufficient = True
 for item in order_ingredients:
-    if item in order_ingredients[item] >= data.resources[item]:
+    if order_ingredients[item] >= resources[item]:
         print(f"Sorry there is not enough {item}")
-        return False
-return True
+        is_sufficient = False
+
 
 
 
