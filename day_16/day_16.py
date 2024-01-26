62d4b681932d1781a535bfa9d5f71c4ac48323ef
Justin <codewithjustin@yahoo.com>
2024-01-26 11:00:38 -0500

    Update day_16.py

---
+++ day_16/day_16.py
@@ -0,0 +1,13 @@
+"""
+Object-Oriented Programming (OOP)
+- Two things to keep in mind when it comes to objects in OOP:
+    1. What the object has (attributes)
+    2. What the object does (methods)
+
+    Example: waiter at a restaurant:
+        Attributes: hold plates, responsible for tables, ect.
+        Methods: take orders, take payments, etc.
+"""
+# 
+
+
