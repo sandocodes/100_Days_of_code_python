2fa47b4cdfbe04c616c666dfc39ba53e08eea234
mr-kollie <codewithjustin@yahoo.com>
2024-01-27 03:48:44 -0500

    coffee_machine_oop.py
    Updated made to coffee_machine and test_file.py.
    Project needs:
    Update profit with each transaction
    Update resources with each transaction

--- day_16/project/coffee_machine_oop.py
+++ day_16/project/coffee_machine_oop.py
@@ -18,11 +18,30 @@ while machine_on:
     else:
         # print(CoffeeMaker().is_resource_sufficient(Menu().find_drink(choice)))  # True
         # Check if item is sufficient
+        # drink = ""
+        # for item in Menu().menu:
+        #     if item.name == choice:
+        #         drink += item.name
         if CoffeeMaker().is_resource_sufficient(Menu().find_drink(choice)):
-
-            print(f"Enough Resources to get {choice}.")
+            # print(f"Enough Resources to get {choice}.")
             # Process Payment
-            # Have enough money ?
+            # Checking if payment is enough
+            # MoneyMachine().process_coins()
 
-        else:
-            print(f"Not Enough Resources to Get {choice}.")
+            # Cost of desired item or specific item cost
+            drink_cost = 0
+            for item in Menu().menu:
+                if item.name == choice:
+                    drink_cost += item.cost
+            print(f"{choice} costs ${drink_cost}")  # Test Purpose
+
+            if MoneyMachine().make_payment(drink_cost):
+                # Make Coffee and Print out result
+                item_ordered = Menu().find_drink(choice)
+                CoffeeMaker().make_coffee(item_ordered)
+
+                # Add to profit
+                change = round(MoneyMachine().money_received - drink_cost, 2)
+                add_to_profit = MoneyMachine().money_received - change
+                MoneyMachine().profit += add_to_profit
+                print(f"{add_to_profit} added to Profit")  # Test Purpose
--- day_16/project/test_file.py
+++ day_16/project/test_file.py
@@ -2,18 +2,57 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+# machine_on = True
+
+# while machine_on:
 item = input("What would you like?: ")
 
+# Cost of desired item or specific item cost
 item_cost = 0
 for i in Menu().menu:
-    item_cost += i.cost
-print(item_cost)
-
-# Specific item cost
-for i in Menu().menu:
     if i.name == item:
-        print(i.name, i.cost)
+        item_cost += i.cost
+        # print(i.name, i.cost)
+print(f"{item} costs ${item_cost}")
+
+#
+if MoneyMachine().make_payment(item_cost):
+    print("Payment made")
+else:
+    print("Insufficient funds")
 
 # Checking if payment is enough
-money_received = MoneyMachine().process_coins()
-print(money_received)
+# MoneyMachine().process_coins()
+# print(money_received)  # Money Received
+
+# List items and their prices
+# for i in Menu().menu:
+#     print(f"{i.name}: {i.cost}")
+
+# print(make_payment)
+# 1. Get Order
+item_order = Menu().find_drink(item)
+print(f"{item_order}")
+
+# Make Coffee
+print(CoffeeMaker().make_coffee(item_order))
+
+# 2. Update Money in Resources
+# Change amount:
+change = round(MoneyMachine().money_received - item_cost, 2)
+amount_to_add = MoneyMachine().money_received - change
+MoneyMachine().profit += amount_to_add
+print(MoneyMachine().profit)
+print(f"{amount_to_add} added to Profit")
+
+# Update resources
+resources = CoffeeMaker().resources
+for i in resources:
+    print(f"{resources}")
+
+for item in item_order.ingredients:
+    CoffeeMaker().resources[item] -= item_order.ingredients[item]
+print(f"Here is your {item_order.name} ☕️. Enjoy!")
+
+for i in resources:
+    print(f"{resources}")
