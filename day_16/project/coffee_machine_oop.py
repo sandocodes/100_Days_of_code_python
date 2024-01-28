311dab5cda3e645ef752a48a27d4977055811f2a
mr-kollie <codewithjustin@yahoo.com>
2024-01-28 00:04:39 -0500

    Update coffee_machine_oop.py
    Project completed, comments added.

--- day_16/project/coffee_machine_oop.py
+++ day_16/project/coffee_machine_oop.py
@@ -2,13 +2,17 @@ from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+# instance of the Menu class
 menu = Menu()
+# instance of the CoffeeMaker class
 coffee_maker = CoffeeMaker()
+# instance of the MoneyMachine class
 money_machine = MoneyMachine()
 
 # is coffee machine on
 is_machine_one = True
 
+# while the coffee machine is still on
 while is_machine_one:
     # user choice of drink (latte/espresso/cappuccino)
     choice = str(input(f"What would you like? ({menu.get_items()}): ")).lower()
@@ -24,7 +28,10 @@ while is_machine_one:
         money_machine.report()
     # if the user typed anything else (latte/espresso/cappuccino)
     else:
-        # Get the drink order
+        # Get the drink order of choice
         order = menu.find_drink(choice)
+
+        # if there is sufficient resources of the order of choice and coin payment is sufficient
         if coffee_maker.is_resource_sufficient(order) and money_machine.make_payment(order.cost):
+            # Make the coffee
             coffee_maker.make_coffee(order)
