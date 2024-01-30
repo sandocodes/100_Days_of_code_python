576ef6ab51adcb5e22ea6bb39cfff3c3a5433c13
mr-kollie <codewithjustin@yahoo.com>
2024-01-30 13:18:20 -0500

    Project
    Project Setup

--- day_17/day_17.py
+++ day_17/day_17.py
@@ -1,10 +1,31 @@
 # Creating Class
 
 class User:
-    pass
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
-user1 = User()
-user1.id = "001"
-user1.name = "Justin"
-print(user1.name)
+user_1 = User(1, "jarso")
+user_2 = User(2, "james")
+user_3 = User(3, "sando")
+
+# user1 decides to follow user2
+user_1.follow(user_2)  # user1 follows user2
+user_1.follow(user_3)  # user1 also follows user3
+
+# output user1 following and followers
+print(user_1.followers)  # expecting 0
+print(user_1.following)  # expecting 2: User 1 is following 2 accounts (user_2 and user_3)
+
+# User 2 & 3 Followers Fount
+print(user_2.followers)  # expecting 1: User_2 is followed by one account (user_1)
+print(user_3.followers)  # expecting 1: User_3 followed by one account (user_1)
+
+
