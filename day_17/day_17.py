49e5aa01daf68b7dede4e108e604551a0f74805d
Justin <codewithjustin@yahoo.com>
2024-01-30 21:43:00 -0500

    Update day_17.py

--- day_17/day_17.py
+++ day_17/day_17.py
@@ -20,14 +20,13 @@ user_3 = User(3, "sando")
 user_1.follow(user_2)  # user1 follows user2
 user_1.follow(user_3)  # user1 also follows user3
 
-
 # output user1 following and followers
 print(f"{user_1.username.title()} has {user_1.followers} followers")  # expecting 0
-print(f"{user_1.username.title()} is following {user_1.following} users.")  # expecting 2: User 1 is following 2 accounts (user_2 and user_3)
+print(
+    f"{user_1.username.title()} is following {user_1.following} users.")  # expecting 2: User 1 is following 2 accounts (user_2 and user_3)
 
 # User 2 & 3 Followers Fount
 print(user_2.followers)  # expecting 1: User_2 is followed by one account (user_1)
 print(user_3.followers)  # expecting 1: User_3 followed by one account (user_1)
-
 
 # Day 17 Completed!
