80be787541af0c6875113219f4dcde6a9aafd2e9
Justin <codewithjustin@yahoo.com>
2024-01-30 21:29:03 -0500

    Quiz Game
    Quiz Game Completed. Added Data from Open Travia DB to data.py file.
    Also made slight changes to main.py file to make code work.

--- day_17/day_17.py
+++ day_17/day_17.py
@@ -20,9 +20,10 @@ user_3 = User(3, "sando")
 user_1.follow(user_2)  # user1 follows user2
 user_1.follow(user_3)  # user1 also follows user3
 
+
 # output user1 following and followers
-print(user_1.followers)  # expecting 0
-print(user_1.following)  # expecting 2: User 1 is following 2 accounts (user_2 and user_3)
+print(f"{user_1.username.title()} has {user_1.followers} followers")  # expecting 0
+print(f"{user_1.username.title()} is following {user_1.following} users.")  # expecting 2: User 1 is following 2 accounts (user_2 and user_3)
 
 # User 2 & 3 Followers Fount
 print(user_2.followers)  # expecting 1: User_2 is followed by one account (user_1)
---
+++ day_17/project/question_model.py
@@ -0,0 +1,8 @@
+class Question:
+    """Takes a text and answer"""
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+
+
