e7d89329be825b1bf81e936c615681a8ca99f285
Justin <codewithjustin@yahoo.com>
2024-01-31 22:02:02 -0500

    Password Generator
    Finalized Password Generator Object-Oriented Programming version.

--- practices/password_generator_oop/password.py
+++ practices/password_generator_oop/password.py
@@ -3,20 +3,53 @@ import random
 
 
 class Password:
-    def __init__(self, num_letters, num_symbols, num_amount):
-        self.how_many_letters = num_letters
-        self.how_many_symbols = num_symbols
-        self.how_many_numbers = num_amount
+    def __init__(self):
+        self.letters = letters
+        self.symbols = symbols
+        self.numbers = numbers
 
-    def generate_letters(self):
-        """ Generate Random Letters (a-zA-Z) """
-        return random.choices(letters, k=self.how_many_letters)
+    # Takes letters list
+    def get_letters(self, n):
+        """ Generate Random Letters (a-zA-Z). This method also takes 'n' as in the number of letters to generate """
+        letters_list = random.choices(self.letters, k=n)
 
-    def generate_symbols(self):
-        """ Generate Random Symbols """
-        return random.choices(symbols, k=self.how_many_symbols)
+        letters = ""
+        for letter in letters_list:
+            letters += letter
+        return letters
 
-    def generate_numbers(self):
-        """ Generate Random Numbers (0 - 9) """
-        return random.choices(numbers, k=self.how_many_numbers)
-    
+    def get_symbols(self, n):
+        """ Generate Random Symbols. This method also takes 'n' as in the number of symbols to generate """
+        symbols_list = random.choices(self.symbols, k=n)
+
+        symbols = ""
+        for symbol in symbols_list:
+            symbols += symbol
+        return symbols
+
+    # Takes numbers list
+    def get_numbers(self, n):
+        """ Generate Random Numbers (0 - 9). This method also takes 'n' as in the amount of numbers to generate"""
+        numbers_list = random.choices(self.numbers, k=n)
+
+        numbers = ""
+        for number in numbers_list:
+            numbers += number
+        return numbers
+
+    def generate_password(self, letters_count, symbols_count, numbers_count):
+        """ The method randomly shuffles the order of  the generated letters, symbols and numbers """
+        random_letters = self.get_letters(letters_count)
+        random_symbols = self.get_symbols(symbols_count)
+        random_numbers = self.get_numbers(numbers_count)
+        password_list = list(f"{random_letters}{random_symbols}{random_numbers}")
+
+        # Shuffle password items in random order
+        random.shuffle(password_list)
+
+        # Turn password into string
+        password = ""
+        for item in password_list:
+            password += item
+
+        return password
--- practices/password_generator_oop/password_generator.py
+++ practices/password_generator_oop/password_generator.py
@@ -1,43 +1,13 @@
 from password import Password
-import random
 
 
 # User inputs
-num_of_letters = int(input("How many letters?: "))
-num_of_symbols = int(input("How many symbols?: "))
-number_amount = int(input("How many numbers?: "))
+letters_count = int(input("How many letters?: "))
+symbols_count = int(input("How many symbols?: "))
+numbers_count = int(input("How many numbers?: "))
 
-# Generate List each of Letters, Symbols and Numbers
-random_letters = Password(num_of_letters, num_of_symbols, number_amount).generate_letters()
-random_symbols = Password(num_of_letters, num_of_symbols, number_amount).generate_symbols()
-random_numbers = Password(num_of_letters, num_of_symbols, number_amount).generate_numbers()
+# New Password Instance
+new_password = Password().generate_password(letters_count, symbols_count, numbers_count)
 
-# Turn Letters, Symbols and Numbers to Strings
-letters = ""
-for letter in random_letters:
-    letters += letter
-
-symbols = ""
-for symbol in random_symbols:
-    symbols += symbol
-
-numbers = ""
-for number in random_numbers:
-    numbers += number
-
-# Generated Password
-generated_password = f"{letters}{symbols}{numbers}"
-
-# Randomize the positions of the Letters, Symbols and Numbers in our password
-# 1. Transform password into list
-list_password = list(generated_password)
-# 2. Shuffle letters, numbers and symbols
-random.shuffle(list_password)
-
-# Turn Password Back to Strings
-password = ""
-for item in list_password:
-    password += item
-
-# Password Output
-print(f"Your Password: {password}")
+# Print the new password
+print(f"Your new password is: {new_password}")
