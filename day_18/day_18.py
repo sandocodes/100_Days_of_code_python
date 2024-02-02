06da291c63bc76dcf0c8fcde0e3191687e4b93f8
mr-kollie <codewithjustin@yahoo.com>
2024-02-02 18:32:51 -0500

    update
    Updated main.py

--- day_18/day_18.py
+++ day_18/day_18.py
@@ -4,35 +4,42 @@ import turtle as t
 screen = t.Screen()
 tim = t.Turtle()
 
-#Example 1. Draw a Sqare 100 x 100
+
+# Example 1. Draw a Square 100 x 100
 def turtle_square():
-    for _ in range(4): 
+    for _ in range(4):
         tim.forward(100)
         tim.right(90)
+
+
 # turtle_square()
 
 
-#Example 2. Draw Dashed Line
+# Example 2. Draw Dashed Line
 def dashed_line():
     for _ in range(15):
         tim.forward(10)
         tim.penup()
         tim.forward(10)
         tim.pendown()
+
+
 # dashed_line()
 
-#Example 3. Drawing Different Shapes with different colors from triangle(3) to pentagon(10)
-    # Square = 90deg angle
-    # Pentagon = 72deg angle
-    # 360 / sides = Working out shape's angle
+# Example 3. Drawing Different Shapes with different colors from triangle(3) to pentagon(10)
+# Square = 90deg angle
+# Pentagon = 72deg angle
+# 360 / sides = Working out shape's angle
 
 
 # Random Colors
 import random
 
 t.colormode(255)
-# color_list = ["red", "blue", "green", "purple", "medium violet red", "chartreuse", "sienna", "indian red", "olive drab", "slate gray"]
-# my_color = random.choice(color_list)
+
+
+# color_list = ["red", "blue", "green", "purple", "medium violet red", "chartreuse", "sienna", "indian red",
+# "olive drab", "slate gray"] my_color = random.choice(color_list)
 
 def random_color():
     r = random.randint(0, 255)
@@ -50,17 +57,21 @@ def random_shape():
             shape_angle = 360 / shape_sides
             tim.forward(100)
             tim.right(shape_angle)
+
+
 # random_shape()
 
 
 # STAR
-            
+
 def star_shape():
     for _ in range(5):
         angle = 145
         tim.forward(100)
         tim.right(angle)
         tim.forward(100)
+
+
 # star_shape()
 
 ############################################
@@ -72,15 +83,15 @@ directions = [0, 90, 180, 270]
 tim.pensize(15)
 tim.speed("fastest")
 
+
 # for _ in range(200):
 #     tim.color(random_color())
 #     tim.forward(30)
 #     tim.setheading(random.choice(directions))
 
 
-#Example 5. Draw a Spirograph - Number of circle, each with a radius of 100 in distance. Use random colors and the Turtle documentation
-# Draw different number of circle
-# Each circle with a radius of 100 in distance
+# Example 5. Draw a Spirograph - Number of circle, each with a radius of 100 in distance. Use random colors and the
+# Turtle documentation Draw different number of circle Each circle with a radius of 100 in distance
 
 
 def draw_spirograph(size_of_gap):
@@ -95,11 +106,8 @@ def draw_spirograph(size_of_gap):
         # Tilt the turtle to the left
         tim.setheading(tim.heading() + size_of_gap)
 
+
 draw_spirograph(5)
-
-
-
-
 
 # screen = Screen()
 screen.exitonclick()
@@ -109,4 +117,5 @@ screen.exitonclick()
 # 2. from....import: from turtle import Turtle or from Turtle import *(as in everything in that module)
 # 3. Aliasing module: import turtle as t
 
-# Installing Modules --> use python package index or pypi to install external modules (or modules no preloaded with python)
+# Installing Modules --> use python package index or pypi to install external modules (or modules no preloaded with
+# python)
--- day_18/project/main.py
+++ day_18/project/main.py
@@ -1,25 +1,41 @@
 import turtle as t
 import colorgram
+import random
 
 brush = t.Turtle()
 screen = t.Screen()
-
-
+t.colormode(255)
 
 # Extract colors from image.jpg
-colors = colorgram.extract('image.jpg', 6)
-print(colors)
+color_list = []
+colors = colorgram.extract('image.jpg', 50)
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    color_list.append(new_color)
 
 
-brush.circle(10)
+def random_color():
+    return random.choice(color_list)
 
 
+brush.setheading(199)
+brush.forward(300)
+brush.setheading(0)
 
+for i in range(5):
+    for _ in range(10):
+        brush.dot(10, random_color())
+        brush.forward(50)
+        brush.dot(10, random_color())
 
-
-
-
-
+    brush.setheading(90)
+    brush.forward(50)
+    brush.setheading(180)
+    brush.forward(500)
+    brush.setheading(0)
 
 
 screen.exitonclick()
