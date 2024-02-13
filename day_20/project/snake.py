26c5cd4ddcb43bf3014a67e725ef9ec5b2caccb6
Justin <codewithjustin@yahoo.com>
2024-02-13 06:46:22 -0500

    Update snake.py file
    Added self.snake_head variable to __init__

--- day_20/project/snake.py
+++ day_20/project/snake.py
@@ -9,6 +9,7 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.snake_head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -25,16 +26,20 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.snake_head.forward(MOVE_DISTANCE)
 
     def up(self):
-        self.segments[0].setheading(90)
+        """ Move Snake Up """
+        self.snake_head.setheading(90)
 
     def down(self):
-        self.segments[0].setheading(270)
+        """ Move Snake Down """
+        self.snake_head.setheading(270)
 
     def right(self):
-        self.segments[0].setheading(0)
+        """ Move Snake Right """
+        self.snake_head.setheading(0)
 
     def left(self):
-        self.segments[0].setheading(180)
+        """ Move Snake Left """
+        self.snake_head.setheading(180)
