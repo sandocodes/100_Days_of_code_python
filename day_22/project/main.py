ec33c71e38b238c9e0b0cd193f7dd073f9b44eb2
Justin <codewithjustin@yahoo.com>
2024-02-15 10:24:38 -0500

    Project Setup
    Created Paddle on the right side

--- day_22/project/main.py
+++ day_22/project/main.py
@@ -1,1 +1,36 @@
 # Create Pong Game
+from turtle import Screen, Turtle
+from paddles import Paddles
+import time
+
+pong = Turtle()
+paddle = Paddles()
+
+pong.hideturtle()
+screen = Screen()
+screen.tracer(0)
+screen.setup(width=1200, height=600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+
+# Disable Animation
+
+
+# Create Pong Board
+pong.color("white")
+pong.penup()
+pong.setheading(90)
+pong.goto(0, 307)
+pong.setheading(270)
+
+for _ in range(30):
+    pong.forward(10)
+    pong.penup()
+    pong.forward(10)
+    pong.pendown()
+
+# Create Paddle
+
+screen.update()
+time.sleep(0.2)
+screen.exitonclick()
---
+++ day_22/project/pong_ball.py
@@ -0,0 +1,5 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+    pass
