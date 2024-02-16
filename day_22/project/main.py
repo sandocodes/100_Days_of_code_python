bd9c75f41d4a97385448526064e0cdf199207446
Justin <codewithjustin@yahoo.com>
2024-02-15 20:08:14 -0500

    Screen Setup, Create Paddle

--- day_22/project/main.py
+++ day_22/project/main.py
@@ -1,36 +1,37 @@
 # Create Pong Game
 from turtle import Screen, Turtle
-from paddles import Paddles
-import time
+from paddle import Paddle
 
-pong = Turtle()
-paddle = Paddles()
-
-pong.hideturtle()
 screen = Screen()
-screen.tracer(0)
-screen.setup(width=1200, height=600)
+# Screen Setup
+screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
+screen.tracer(0)
 
-# Disable Animation
+r_paddle = Paddle((-350, 0))
+l_paddle = Paddle((-350, 0))
 
 
-# Create Pong Board
-pong.color("white")
-pong.penup()
-pong.setheading(90)
-pong.goto(0, 307)
-pong.setheading(270)
+# Go Up function
+def go_up(self):
+    new_y = self.paddle.ycor() + 20
+    self.paddle.goto(self.paddle.xcor(), new_y)
 
-for _ in range(30):
-    pong.forward(10)
-    pong.penup()
-    pong.forward(10)
-    pong.pendown()
 
-# Create Paddle
+# Go Down function
+def go_down(self):
+    new_y = self.paddle.ycor() - 20
+    self.paddle.goto(self.paddle.xcor(), new_y)
 
-screen.update()
-time.sleep(0.2)
+
+# Move Paddle Up and Down
+screen.listen()
+screen.onkey(Paddle((-350, 0)).go_up, "Up")
+screen.onkey(Paddle((-350, 0)).go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
 screen.exitonclick()
