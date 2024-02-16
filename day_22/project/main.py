7d9abee367cca952c9336330119a009d48b251dc
Justin <codewithjustin@yahoo.com>
2024-02-15 21:51:59 -0500

    Collision with Right Paddle

--- day_22/project/main.py
+++ day_22/project/main.py
@@ -4,7 +4,6 @@ from paddle import Paddle
 from ball import Ball
 import time
 
-
 screen = Screen()
 # Screen Setup
 screen.setup(width=800, height=600)
@@ -28,6 +27,14 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
     ball.move()
+
+    # Detect collision with top or bottom walls
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with the paddle(s)
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340:
+        ball.bounce_x()
 
 
 screen.exitonclick()
