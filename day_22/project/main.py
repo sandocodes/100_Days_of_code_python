2004e5eb9627e254998ba62b13b81ba6fb461e33
Justin <codewithjustin@yahoo.com>
2024-02-15 20:16:32 -0500

    Added two paddles to game
    Added right and left paddles to game.
    Up and Down arrow keys move right paddle while "w" and "s" keys move left paddle up and down respectively.

--- day_22/project/main.py
+++ day_22/project/main.py
@@ -9,26 +9,15 @@ screen.bgcolor("black")
 screen.title("Pong Game")
 screen.tracer(0)
 
-r_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
-
-
-# Go Up function
-def go_up(self):
-    new_y = self.paddle.ycor() + 20
-    self.paddle.goto(self.paddle.xcor(), new_y)
-
-
-# Go Down function
-def go_down(self):
-    new_y = self.paddle.ycor() - 20
-    self.paddle.goto(self.paddle.xcor(), new_y)
-
 
 # Move Paddle Up and Down
 screen.listen()
-screen.onkey(Paddle((-350, 0)).go_up, "Up")
-screen.onkey(Paddle((-350, 0)).go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
--- day_22/project/paddle.py
+++ day_22/project/paddle.py
@@ -4,9 +4,18 @@ from turtle import Turtle
 class Paddle(Turtle):
     def __init__(self, position):
         super().__init__()
+        self.shape("square")
         self.color("white")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
         self.goto(position)
 
+    # Go Up function
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
 
+    # Go Down function
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
