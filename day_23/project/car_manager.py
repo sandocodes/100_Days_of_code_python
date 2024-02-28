1ee67f285458d3d6363b2a3ab723cab4a4f9afd7
Justin <codewithjustin@yahoo.com>
2024-02-28 01:34:10 -0500

    Changes

--- day_23/project/car_manager.py
+++ day_23/project/car_manager.py
@@ -1,11 +1,19 @@
 from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.cars = []
     
 
     def create_cars(self):
-        pass
+        for _ in range(6):
+            self.shape("square")
+            self.color(random.choice(COLORS))
+            self.shapesize(stretch_wid=2, stretch_len=4)
+            self.goto(random.randint(0, 200), random.randint(-200, 200))
--- day_23/project/main.py
+++ day_23/project/main.py
@@ -4,34 +4,21 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
-# Temp import
-import random
+
+cars = CarManager()
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
-################# Create cars #####################
-rand_position_x = random.randint(-200, 200)
-rand_position_y = random.randint(-200, 200)
-rand_position = (rand_position_x, rand_position_y)
-COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
-cars = []
-
-for _ in range(len(COLORS)):
-    new_car = Turtle("square")
-    new_car.color(random.choice(COLORS))
-    new_car.goto(rand_position)
-    cars.append(new_car)
 
 
-
-
-###################################################
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    
 
 
 
