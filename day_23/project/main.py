b016788c772c520ac9cf25b1556fcb944ae53894
Justin <codewithjustin@yahoo.com>
2024-02-27 23:48:09 -0500

    Update main.py

--- day_23/project/main.py
+++ day_23/project/main.py
@@ -11,20 +11,23 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
-# Create cars
-rand_position_x = random.randint(1, 5)
-rand_position_y = random.randint(1, 5)
-position = (rand_position_x, rand_position_y)
+################# Create cars #####################
+rand_position_x = random.randint(-200, 200)
+rand_position_y = random.randint(-200, 200)
+rand_position = (rand_position_x, rand_position_y)
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 cars = []
 
-for car in range(len(COLORS)):
+for _ in range(len(COLORS)):
     new_car = Turtle("square")
     new_car.color(random.choice(COLORS))
-    new_car.goto(position)
+    new_car.goto(rand_position)
+    cars.append(new_car)
 
 
-#########################################
+
+
+###################################################
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
