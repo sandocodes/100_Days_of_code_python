214a2733b622c490fa59446e7762a5902ecc6499
Justin <codewithjustin@yahoo.com>
2024-02-28 01:43:48 -0500

    Minor Changes (No importance)

--- day_23/project/car_manager.py
+++ day_23/project/car_manager.py
@@ -8,12 +8,7 @@ MOVE_INCREMENT = 10
 class CarManager(Turtle):
     def __init__(self):
         super().__init__()
-        self.cars = []
     
 
     def create_cars(self):
-        for _ in range(6):
-            self.shape("square")
-            self.color(random.choice(COLORS))
-            self.shapesize(stretch_wid=2, stretch_len=4)
-            self.goto(random.randint(0, 200), random.randint(-200, 200))
+        pass
--- day_23/project/main.py
+++ day_23/project/main.py
@@ -1,23 +1,37 @@
 import time
 from turtle import Screen, Turtle
 from player import Player
-from car_manager import CarManager
+from car_manager import CarManager, COLORS
 from scoreboard import Scoreboard
+import random
 
 
-cars = CarManager()
+car = CarManager()
 
 
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.tracer(0)
+# screen.tracer(0)
+
+
+# ########################## #
+
+
+new_car = Turtle("square")
+new_car.color(random.choice(COLORS))
+new_car.shapesize(stretch_wid=2, stretch_len=4)
+new_car.penup()
+new_car.goto(random.randint(0, 200), random.randint(-200, 200))
+
+
+# ########################## #
 
 
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
-    screen.update()
+    # screen.update()
     
 
 
