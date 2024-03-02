3facde274cd55fe92c5ddb92f896decfa5ea7692
mr-kollie <codewithjustin@yahoo.com>
2024-03-02 02:07:18 -0500

    project main.py
    Removed spaces from left and right of the invited names on line17. Code works as expected.

--- day_24/project/mail_merge/main.py
+++ day_24/project/mail_merge/main.py
@@ -6,21 +6,23 @@
 # Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
 # Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
 # Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+import os
+
 
 invite_names = []  # invited names
 replacement_txt = "[Test]"
 
 with open('./Input/Names/invited_names.txt') as invites:
     for name in invites:
-        invite_names.append(name)
+        invite_names.append(name.strip())
 
 for inv_name in invite_names:
-    with open(f'./Output/ReadyToSend/{inv_name}.txt', 'w') as example_file:
+    with open(f'./Output/ReadyToSend/{inv_name.strip()}.txt', 'w') as example_file:
         # Write the contents of the 'starter_letter.txt' to example file
         with open("./Input/Letters/starting_letter.txt") as starter_letter:
             letter_lines = starter_letter.readlines()
+            print(letter_lines)
             for line in letter_lines:
-                formatted_lines = f"{line.strip()}\n"
+                formatted_lines = line.replace('[name]', f"{inv_name}").rstrip()
                 print(formatted_lines)
-                example_file.write(f"{formatted_lines.replace('[name]', inv_name)}")
-        
+                example_file.write(f"{formatted_lines}\n")
