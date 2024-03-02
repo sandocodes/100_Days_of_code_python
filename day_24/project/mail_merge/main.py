c4677812dc91fafa7c4fce70e195c75033afd977
mr-kollie <codewithjustin@yahoo.com>
2024-03-02 10:49:21 -0500

    Day24 project/main.py
    Day24 Project Complete. Code works as expected.

--- day_24/project/mail_merge/main.py
+++ day_24/project/mail_merge/main.py
@@ -7,13 +7,18 @@
 # Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
 # Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
-# invited names
-invite_names = []
 
-with open("./Output/ReadyToSend/example.txt", "w") as example_file:
-    # Write the contents of the 'starter_letter.txt' to example file
-    with open("./Input/Letters/starting_letter.txt") as starter_letter:
-        letter_lines = starter_letter.readlines()
-        for line in letter_lines:
-            line.replace("[name]", replacement_txt)
-            example_file.write(f"{line.strip()}\n")
+all_invited = []
+
+with open("./Input/Names/invited_names.txt") as invited_names:
+    for name in invited_names:
+        all_invited.append(name.strip())
+
+for invited in all_invited:
+    with open(f"./Output/ReadyToSend/letter_for_{invited}.txt", "w") as person_invited:
+        # Write the contents of the 'starter_letter.txt' to the new files created.
+        with open("./Input/Letters/starting_letter.txt") as starter_letter:
+            letter_lines = starter_letter.readlines()
+            for line in letter_lines:
+                formatted_lines = line.replace("[name]", f"{invited}").strip()
+                person_invited.write(f"{formatted_lines}\n")
