d179ea7cd830b8b71384c51d7261e4707b62818c
Justin <codewithjustin@yahoo.com>
2024-03-06 13:56:41 -0500

    Update day_25.py
    Pandas DataFrame and Series (Rows and Columns)

--- day_25/day_25.py
+++ day_25/day_25.py
@@ -13,4 +13,32 @@ import pandas
 
 # Pandas Library
 data = pandas.read_csv("./weather_data.csv")
-print(data)
+# print(data)
+
+# Pandas DataFrames and Series: Working with Rows and Columns
+data_dict = data.to_dict()
+# print(data_dict)
+
+# Data Series
+# temp = data["temp"] #to get the data in the temp column. Can also be done using data.temp
+# average_temp = temp.mean()
+# max_temp = temp.max()
+# min_temp = temp.min()
+# print(f"Temperatures: {temp}")
+# print(f"Average temp: {round(average_temp, 2)}")
+# print(f"Max temp: {round(max_temp, 2)}")
+# print(f"Min temp: {round(min_temp, 2)}")
+
+# Selecting specific row data
+monday = data[data.day == "Monday"]
+print(monday)
+
+# Eg. Get the row where the temperature is at its maximum
+maximum_temp = data[data.temp == data.temp.max()]
+# print(maximum_temp)
+
+# Eg: Convert Monday's temp value to Frenheit
+monday = data[data.day == "Monday"]
+mond_temp = monday.temp[0]
+mond_temp_f = mond_temp * 9/5 + 32
+print(mond_temp_f)
