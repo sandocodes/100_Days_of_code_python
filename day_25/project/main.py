d3654942695d833bc07fdc3ee6cae34b09c07135
Justin <codewithjustin@yahoo.com>
2024-03-06 19:53:25 -0500

    update main.py in project folder
    Started project (u.s. states game) code.

---
+++ day_25/project/main.py
@@ -0,0 +1,13 @@
+import turtle
+
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+game_image = "blank_states_img.gif"
+screen.addshape(game_image)
+
+
+turtle.shape(game_image)
+
+
+screen.exitonclick()
