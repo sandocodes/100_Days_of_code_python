a901605079e4f7cc008c7df14e0997e60e97334e
mr-kollie <codewithjustin@yahoo.com>
2024-03-13 13:09:52 -0400

    updated main.py in Day_25 Project folder
    Completed the Nato Alphabets challenges.

--- day_26/project/main.py
+++ day_26/project/main.py
@@ -1,11 +1,17 @@
 import pandas as pd
 
-csv_data = pd.read_csv('/nato_phonetic_alphabet.csv')
-print(csv_data)
 
+data = pd.read_csv('./nato_phonetic_alphabet.csv')
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+# Todo 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# Todo 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Enter a word: ").upper()
+
+split_word = [letter for letter in word]
+
+nato_code = [phonetic_dict[letter] for letter in split_word]
+print(nato_code)
 
