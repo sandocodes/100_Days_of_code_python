e3a1446ccf37f99592d0194ceccbc6abc9d41e7b
Justin <codewithjustin@yahoo.com>
2024-03-31 21:05:07 -0400

    Update day_27.py

--- day_27/day_27.py
+++ day_27/day_27.py
@@ -1,18 +1,1 @@
 # Learning Tkinter a Python GUI library
-import tkinter
-
-window = tkinter.Tk()
-
-window.title("My First GUI Program")
-window
-window.minsize(width=500, height=300)
-
-# Label
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack()
-
-
-
-
-
-window.mainloop()
